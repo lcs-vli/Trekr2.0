c241a547a4c91b5e3e20f79d26883ac0d69f0155
Victoria Li <VLi@lcs.on.ca>
2021-01-28 11:22:11 -0500

    Initial Commit

---
+++ Shared/Trekr2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Trekr2_0App.swift
+//  Shared
+//
+//  Created by Li, Muchen on 2021/1/28.
+//
+
+import SwiftUI
+
+@main
+struct Trekr2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
