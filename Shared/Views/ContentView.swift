49fd01b98d715e7020563c974412d29007719196
Victoria Li <VLi@lcs.on.ca>
2021-01-28 16:22:48 -0500

    added navigation links to the locations page

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -10,19 +10,22 @@ import SwiftUI
 struct Locations: Identifiable {
     var id = UUID()
     var name = String()
+    var country = String()
 }
 
-let testData: [Locations] = [Locations(name: "Beijing"),Locations(name: "Toronto"),Locations(name: "Seattle")]
+let testData: [Locations] = [Locations(name: "Beijing", country: "China"),Locations(name: "Toronto", country: "Canada"),Locations(name: "Seattle", country: "USA")]
 
 struct ContentView: View {
     var body: some View {
         
         List(testData) { location in
             
-            VStack(alignment: .leading) {
-                Text(location.name)
-                Text("location.country")
-                    .font(.subheadline)
+            NavigationLink(destination: LocationDetail()){
+                VStack(alignment: .leading) {
+                    Text(location.name)
+                    Text(location.country)
+                        .font(.subheadline)
+                }
             }
             
         }
