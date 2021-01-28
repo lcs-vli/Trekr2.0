2f5c27e7c989be22e6281ea5e38fe10a5e16a372
Victoria Li <VLi@lcs.on.ca>
2021-01-28 11:54:30 -0500

    created location view

---
+++ Shared/Model/LocationStore.swift
@@ -0,0 +1,19 @@
+//
+//  LocationStore.swift
+//  Trekr2.0 (iOS)
+//
+//  Created by Li, Muchen on 2021/1/28.
+//
+
+import Foundation
+
+class LocationStore: ObservableObject {
+
+    @Published var locations: [Location]
+    
+    init(locations: [Location] = []){
+        self.locations = Location
+    }
+}
+
+let testStore = LocationStore(location: testData)
