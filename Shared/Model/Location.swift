d3ffd575bc0c2c2278a7a8f6b767e80c14ae0acb
Victoria Li <VLi@lcs.on.ca>
2021-03-03 20:53:17 -0500

    removed the coordinates from Location. finish working on saving the locations permanently then add the coordinates

--- Shared/Model/Location.swift
+++ Shared/Model/Location.swift
@@ -14,14 +14,14 @@ struct Location: Decodable, Identifiable {
     var image: String
     var rating: Rating
     var description: String
-    var latitude: Double
-    var longitude: Double
+    //var latitude: Double
+    //var longitude: Double
 }
 
 let testData = [
-    Location(name: "Toronto", country: "Canada", image: "toronto", rating: .five, description: "A really nice place. Lots of malls.", latitude: -43.6532, longitude: -79.3832),
-    Location(name: "Beijing", country: "China", image: "beijing", rating: .five, description: "My home town", latitude: 35.8617, longitude: 104.1954),
-    Location(name: "Seattle", country: "U.S.", image: "seattle", rating: .three, description: "My cousin's place", latitude: 37.0902, longitude: 95.7129),
-    Location(name: "Tokyo", country: "Japan", image: "tokyo", rating: .two, description: "We go there almost every break. It is so much fun.", latitude: 36.2048, longitude: 138.2529),
-    Location(name: "Chengde", country: "China", image: "chengde", rating: .one, description: "We went there this summer with our dog.", latitude: 35.8617, longitude: 104.1954)
+    Location(name: "Toronto", country: "Canada", image: "toronto", rating: .five, description: "A really nice place. Lots of malls."),
+    Location(name: "Beijing", country: "China", image: "beijing", rating: .five, description: "My home town"),
+    Location(name: "Seattle", country: "U.S.", image: "seattle", rating: .three, description: "My cousin's place"),
+    Location(name: "Tokyo", country: "Japan", image: "tokyo", rating: .two, description: "We go there almost every break. It is so much fun."),
+    Location(name: "Chengde", country: "China", image: "chengde", rating: .one, description: "We went there this summer with our dog.")
 ]
--- Shared/Views/WorldMap.swift
+++ Shared/Views/WorldMap.swift
@@ -5,46 +5,46 @@
 //  Created by Li, Muchen on 2021/1/28.
 //
 
-import MapKit
-import SwiftUI
-
-struct WorldMap: View {
-    
-    //get a referecen to the location store
-    //this is a derived value passed as a parameter
-    @ObservedObject var store: LocationStore
-    
-    //centre on LCS, wide enough to show most of eastern Nother America
-    @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 44.43922, longitude: -78.26571),
-                                           span: MKCoordinateSpan(latitudeDelta: 40, longitudeDelta: 40))
-    
-    var body: some View {
-        
-        Map(coordinateRegion: $region, annotationItems: store.locations) { location in
-            
-            MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)) {
-                
-                NavigationLink(destination: CountryLocation()){
-                    Image(location.country)
-                        .resizable()
-                        .cornerRadius(10)
-                        .frame(width: 80, height: 40)
-                        .shadow(radius: 3)
-                }
-                
-            }
-            
-        }
-        .navigationTitle("Map")
-    
-    }
-}
-
-struct WorldMap_Previews: PreviewProvider {
-    static var previews: some View {
-        NavigationView {
-            WorldMap(store: testStore)
-        }
-    }
-}
+//import MapKit
+//import SwiftUI
+//
+//struct WorldMap: View {
+//
+//    //get a referecen to the location store
+//    //this is a derived value passed as a parameter
+//    @ObservedObject var store: LocationStore
+//
+//    //centre on LCS, wide enough to show most of eastern Nother America
+//    @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 44.43922, longitude: -78.26571),
+//                                           span: MKCoordinateSpan(latitudeDelta: 40, longitudeDelta: 40))
+//
+//    var body: some View {
+//
+//        Map(coordinateRegion: $region, annotationItems: store.locations) { location in
+//
+//            MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)) {
+//
+//                NavigationLink(destination: CountryLocation()){
+//                    Image(location.country)
+//                        .resizable()
+//                        .cornerRadius(10)
+//                        .frame(width: 80, height: 40)
+//                        .shadow(radius: 3)
+//                }
+//
+//            }
+//
+//        }
+//        .navigationTitle("Map")
+//
+//    }
+//}
+//
+//struct WorldMap_Previews: PreviewProvider {
+//    static var previews: some View {
+//        NavigationView {
+//            WorldMap(store: testStore)
+//        }
+//    }
+//}
 
